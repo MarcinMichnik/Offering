9cd10f7f106696f719e8aa9e7f9aa16907ac0bb1
MarcinMichnik <marcin.michnik@o2.pl>
2021-07-21 22:06:19 +0200

    :sparkles: offer model

---
+++ OfferingWebsite/Models/Offer.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace OfferingWebsite
+{
+    public class Offer
+    {
+        public DateTime DateCreated { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Owner { get; set; }
+    }
+}
