a328bdef6e0b7656812fcc190007acb2a307f929
MarcinMichnik <marcin.michnik@o2.pl>
2021-10-04 20:23:37 +0200

    :sparkles: User system working version

--- OfferingWebsite/Startup.cs
+++ OfferingWebsite/Startup.cs
@@ -29,6 +29,8 @@ namespace OfferingWebsite
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
             // use sql server db in production and sqlite db in development
             if (_env.IsProduction())
                 services.AddDbContext<DataContext>();
