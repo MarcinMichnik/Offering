620cf82b6b9c800ff2b7fa5f5af33f2693bb43e5
MarcinMichnik <marcin.michnik@o2.pl>
2021-10-20 16:41:28 +0200

    :sparkles: login link at homepage

--- OfferingWebsite/ClientApp/src/components/Home.js
+++ OfferingWebsite/ClientApp/src/components/Home.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react';
 
 export class Home extends Component {
   static displayName = Home.name;
+  url = "/login";
 
   render () {
     return (
       <div>
-            <h1>Login</h1>
+            <h1>Homepage</h1>
+            <a href={this.url}>
+                <h3>Login</h3>
+            </a>
       </div>
     );
   }
